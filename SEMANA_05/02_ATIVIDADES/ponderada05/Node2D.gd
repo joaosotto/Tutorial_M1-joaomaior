10d11ddccef0ded3d0e7f293e655a690ed025a1a
joaosotto <joao.maior@sou.inteli.edu.br>
2023-03-04 20:36:18 -0300

    ponderada semana 05

--- SEMANA_05/02_ATIVIDADES/ponderada05/Node2D.gd
+++ SEMANA_05/02_ATIVIDADES/ponderada05/Node2D.gd
@@ -1,28 +1,26 @@
 extends Node2D
 
-var teste = false
-var valor = 0
+#declara variáveis
 var numero = 0
-var lista = []
 var nome
+var lista = []
 var tamanho = len(lista)
 
 func _on_Button_pressed():
-	#Coletando dados inseridos pelo usuário
+	#coletando dados inseridos pelo usuário
 	numero = int($LineEditnum.text)
 	nome = str($LineEditnome.text)
 
 
 func _on_Button2_pressed():
-	#Incrementando o número inserido pelo usuário
+	#incrementando o número inserido pelo usuário e imprimindo na cena 
 	for i in range(10):
 		numero+=i
 		lista.append(numero)
 	$saidaButton2.text = str(lista)
 
 func _on_Button3_pressed():
-	#Mudando o nome do usuário de acordo com os dados da lista
-	#Se houver algum número ímpar, o nome deve adicionar "baldo" ao final
+	#adicionando "baldo" ao nome se houver número impar na lista. Imprimindo na tela 
 	var i = 0
 	while(i<tamanho):
 		if lista[i]%2 == 0:
